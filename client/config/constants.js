84c0a93bae792b5ff5a55b6f2e59d4ea71725157
Chris Oyler <christopher.oyler@gmail.com>
2015-06-27 23:26:00 -0700

    checking in

--- client/config/constants.js
+++ client/config/constants.js
@@ -1,5 +1,5 @@
 'use strict';
 
 angular.module('address')
-.constant('firebaseUrl', 'https://addresslog.firebaseio.com/');
+.constant('firebaseUrl', 'https://dostuff.firebaseio.com/');
 //this simply declares the firebase web address as a variable that can be used anywhere in the program
